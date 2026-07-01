2e018dd352d571df99ad14f19769bcea35a8bc4b
pcodedynamics <pcodedynamics@gmail.com>
2026-07-01 21:28:07 +0700

    feat(android): initialize Android project structure and configuration
    - Added essential Gradle files for project setup, including `build.gradle.kts`, `gradle.properties`, and `settings.gradle.kts`.
    - Created `.gitignore` files to exclude build artifacts and IDE-specific files.
    - Implemented the main application structure with `MainActivity`, UI themes, and resources for colors and strings.
    - Included initial test cases for both instrumented and unit tests to ensure basic functionality.
    - Established drawable resources for app icons and backgrounds, enhancing visual identity.

---
+++ platforms/android/app/src/main/java/app/funput/funput/MainActivity.kt
@@ -0,0 +1,47 @@
+package app.funput.funput
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.funput.funput.ui.theme.FunputTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FunputTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FunputTheme {
+        Greeting("Android")
+    }
+}
